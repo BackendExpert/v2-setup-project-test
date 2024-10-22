b72bf4a95725df467a59dab7f881e58f2deb80de
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-22 19:35:25 +0530

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const { exec } = require('child_process');
+
+
+async function YesFunc() {
+    console.log("Create Clinet and Server in Samefolder")
+}
+
+async function NoFunc() {
+    console.log("Create Clinet and Server in Samefolder")
+}
+
+
+async function main() {
+    try{
+        const answers = await inquirer.prompt([
+            {
+              type: 'list',
+              name: 'createClientServer',
+              message: 'Create Client and Server in Same Folder(Repo) :',
+              choices: ['Yes', 'No'],
+            },
+        ]);   
+
+        const { createClientServer } = answers;
+        
+        switch(createClientServer){
+            case 'Yes':
+                await YesFunc()
+            
+            case 'No':
+                await NoFunc()
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+main()
