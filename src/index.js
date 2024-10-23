5f438b556ac797a984d65c4530441e14f46a6def
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-23 08:22:41 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -4,50 +4,30 @@ const inquirer = require('inquirer');
 const { exec } = require('child_process');
 const path = require('path')
 const currentDir = process.cwd();
-const fs = require('fs')
+const fs = require('fs');
+const { exit } = require('process');
 
-async function CreateClientFolderandMoveFiles() {
-    try{
-        if (!fs.existsSync(clientDir)) {
-            fs.mkdirSync(clientDir);
-        }
-
-        const files = fs.readdirSync(currentDir);
-
-        files.forEach(file => {
-            if (file !== 'client') {
-              const oldPath = path.join(currentDir, file);
-              const newPath = path.join(clientDir, file);
-              
-              fs.renameSync(oldPath, newPath);
+async function execPromise(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(`Error executing command: ${error.message}`);
+            } else if (stderr) {
+                reject(`Error: ${stderr}`);
+            } else {
+                resolve(stdout);
             }
         });
-
-        console.log('All files moved to "client" folder.');
-        process.chdir(clientDir); 
-    }
-    catch(err){
-        console.log(err)
-    }
+    });
 }
+
 
 async function CreateReactViteProject () {
     try{
+        const output = await execPromise('npm create vite@latest client -- --template react');
+        console.log(`Vite project created successfully: ${output}`);
 
-
-        exec('npm create vite@latest client -- --template react cd client', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error executing command: ${error.message}`);
-                return;
-            }
-        
-            if (stderr) {
-                console.error(`Error: ${stderr}`);
-                return;
-            }
-        
-            console.log(`Output: ${stdout}`);
-        })
+        await DeleteUnnecessaryFilesandFolders();
 
     }
 
@@ -69,13 +49,16 @@ async function DeleteUnnecessaryFilesandFolders (){
             fs.unlinkSync(packageLockJsonPath)
         }
         if (fs.existsSync(nodeModulesPath)) {
-            fs.rm(nodeModulesPath, { recursive: true, force: true }, (err) => {
-                if (err) {
-                    console.error(`Error deleting folder: ${err.message}`);
-                } else {
-                    console.log(`Folder deleted: ${nodeModulesPath}`);
-                }
-            });
+            fs.rmSync(nodeModulesPath, { recursive: true, force: true });
+            console.log(`Folder deleted: ${nodeModulesPath}`);    
+            await MovetoClientAndInstallNPMs()
+            await InstallTailwindCSS()
+            console.log('Curent WD ' + process.cwd())        
+        }
+        else{
+            await MovetoClientAndInstallNPMs()
+            await InstallTailwindCSS()
+            console.log('Curent WD ' + process.cwd())
         }
     }
     catch(err){
@@ -83,9 +66,29 @@ async function DeleteUnnecessaryFilesandFolders (){
     }
 }
 
-async function MovetoClientAndInstallNPMs () {
-    try{
+async function MovetoClientAndInstallNPMs() {
+    try {
+        const clientDir = path.join(currentDir, 'client');
 
+        if (fs.existsSync(clientDir)) {
+            process.chdir(clientDir); 
+            console.log(`Changed working directory to: ${process.cwd()}`);
+
+            const output = await execPromise('npm install');
+            console.log(`NPM packages installed in client directory: ${output}`);
+        } else {
+            console.error(`Client directory not found: ${clientDir}`);
+        }
+    } catch (err) {
+        console.error(`Error during changing directory or installing NPM packages: ${err.message}`);
+    }
+}
+
+async function InstallTailwindCSS () {
+    try{
+        const tailwindInstall = await execPromise('npm install -D tailwindcss postcss autoprefixer');
+        const tailwindcssInit = await execPromise('npx tailwindcss init -p');
+        console.log(`: ${output}`);
     }
     catch(err){
         console.log(err)
@@ -108,8 +111,8 @@ async function main() {
         switch(createClientServer){
             case 'Yes':
                 await CreateReactViteProject()
-                await DeleteUnnecessaryFilesandFolders()
-                await MovetoClientAndInstallNPMs()
+                exit;
+
                 break
             
             case 'No':
